823123728e3053c3f44d14ff5330610e48875383
ismael1987 <asmael-1987@hotmail.com>
2018-06-14 12:34:21 +0200

    improve products list with css and connect the button for it

--- src/main/java/com/emusicstore/dao/ProductDao.java
+++ src/main/java/com/emusicstore/dao/ProductDao.java
@@ -21,8 +21,19 @@ public class ProductDao {
         product1.setUnitInStock(11);
         product1.setProductManufacturer("fender");
 
+        Product product2= new Product();
+        product2.setProductName("recordl");
+        product2.setProductCategory("record");
+        product2.setProductDescription("this is a nice thing");
+        product2.setProductPrice(25);
+        product2.setProductCondition("new");
+        product2.setProductStatus("active");
+        product2.setUnitInStock(51);
+        product2.setProductManufacturer("EMI");
+
         productList = new ArrayList<Product>();
         productList.add(product1);
+        productList.add(product2);
         return productList;
     }
 }
